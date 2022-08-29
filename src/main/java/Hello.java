bf5df407d1d94b52cb7c282bc25050fb650ad961
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-08-29 00:57:31 -0500

    receive user input

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String who = in.nextLine();
+        System.out.println("Hello" + who);
     }
 }
